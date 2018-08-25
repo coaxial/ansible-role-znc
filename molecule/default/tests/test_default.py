e1fb3bbc66e908afa2f38e3dd3d6186cc162b63d
coaxial <hi@64b.it>
2018-08-25 17:09:36 -0400

    Remove example test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,14 +6,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_example(host):
-    file = host.file('/etc/hosts')
-
-    assert file.exists
-    assert file.user == 'root'
-    assert file.group == 'root'
-
-
 def test_pkg_install(host):
     p = host.package("znc")
 
